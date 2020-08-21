d09c6dfd0a4759d3a62749e66a298d4c6aa52498
Your Name <you@example.com>
2020-08-21 18:35:22 -0400

    home y navar

---
+++ mulebris/src/app/profile-specialist/profile-specialist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-specialist',
+  templateUrl: './profile-specialist.component.html',
+  styleUrls: ['./profile-specialist.component.css']
+})
+export class ProfileSpecialistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
